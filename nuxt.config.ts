34a156f5520efde19523dadb4857751247d591f1
Pavel Lazar <pavel@alazara.com>
2024-02-06 10:57:36 -0500

    chore: playground reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "awsAmplify",
-  },
-  routeRules: {
-    // all routes (by default) will be revalidated every 60 seconds, in the background
-    "/**": { isr: 60 },
-    // this page will be generated on demand and then cached permanently
-    "/static": { isr: true },
-    // this page is generated at build time and cached permanently
-    "/prerendered": { prerender: true },
-    // this page will be always fresh
-    "/dynamic": { isr: false },
-    // you can do lots more with route rules too!
-    "/redirect": { redirect: "/static" },
-    "/headers": { headers: { "x-magic-of": "nuxt and vercel" } },
-    "/spa": { ssr: false },
-  },
-});
+  devtools: { enabled: true }
+})
--- server/api/info.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(event => {
-  const config = useRuntimeConfig()
-
-  return {
-    region: config.region,
-  }
-})
